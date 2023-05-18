8b5969cc51c749138bfb9f2a7f079f52fee914f2
cliquebenito <skrollbrad@mail.ru>
2023-05-18 12:16:29 +0300

    retrofit next

---
+++ app/src/main/java/com/example/cryproapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.cryproapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.cryproapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        val navController = navHostFragment!!.findNavController()
+
+
+        val popupMenu = PopupMenu(this,binding.bottomBar)
+        popupMenu.inflate(R.menu.bottom_nav_menu8)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+    }
+}
